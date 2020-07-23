e4deea46bdb4e2735d828ea13b3c10d8b95da6d1
Chasen.xie <chasen.xie@trustasia.com>
2020-07-23 13:49:42 +0800

    feat(test): test

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 // var expect = require('chai').expect;
 var adapter = require('./adapter.spec.js');
 
+// 测试
 describe("Promises/A+ Tests", function () {
   require("promises-aplus-tests").mocha(adapter);
 });
